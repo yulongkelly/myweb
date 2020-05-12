a5e789aa2efeab2748c52a6161652a473056dc37
Kelly Wu <kelly.wu@edu.uwaterloo.ca>
2020-05-12 13:50:05 -0400

    add a project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,7 +3,6 @@ import { Card, CardGroup, Col } from "react-bootstrap";
 import five from "./five-in-a-row.png";
 import academic from "./academic.png";
 import habit1 from "./habit1.png";
-import habit2 from "./habit2.png";
 
 class Project extends Component {
   render() {
@@ -13,9 +12,7 @@ class Project extends Component {
           <Card.Img variant="top" src={five} className="projectImg" />
           <Card.Body>
             <Card.Title>Five In a Row</Card.Title>
-            <Card.Text>
-              This is a Chinese game and also my high school final project
-            </Card.Text>
+            <Card.Text>This is an abstract strategy board game.</Card.Text>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
@@ -25,24 +22,24 @@ class Project extends Component {
           <Card.Img variant="top" src={academic} className="projectImg" />
           <Card.Body>
             <Card.Title>Academic Anonymous</Card.Title>
-            <Card.Text>Building this during Hack the Valley!</Card.Text>
+            <Card.Text>
+              Academics Anonymous is a live QA session that allows students to
+              continuously ask questions during in-class lectures.
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
         <Card>
-          {/* <Card.Img variant="top" src={habit1} className="projectImg" /> */}
-          {/* <Col>
-              <Card.Img variant="top" src={habit2} />
-            </Col> */}
+          <Card.Img variant="top" src={habit1} className="projectImg" />
 
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Habit Haven</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
+              This app tackles the issue of self-discipline by setting
+              short-term goals for the user which will lead them towards a more
+              productive path.
             </Card.Text>
           </Card.Body>
           <Card.Footer>
